cf2fb2d28205547666f8be46cd7f40a8279e0d49
Austin Ossy <austinossy20@gmail.com>
2024-12-19 10:30:04 +0100

    handle transaction error

--- client/src/pages/ticketsreceipt.js
+++ client/src/pages/ticketsreceipt.js
@@ -36,6 +36,8 @@ const ticketsreceipt = () => {
       router.push("/");
     }
   }, [transaction]);
+  
+  
 
   return (
     <PrivateRoute>
@@ -45,56 +47,83 @@ const ticketsreceipt = () => {
             Online Tickets
           </p>
           <p className="text-xl sm:text-[40px] font-bold text-center leading-snug">
-            Ticket purchased confirmation
+            Ticket purchase confirmation
           </p>
         </div>
 
-        <div className="mx-auto w-full my-3 flex flex-col gap-2 items-center">
-          <StyledImage src="/img/ticket-success.svg" />
+        {success === true && (
+          <div className="mx-auto w-full my-3 flex flex-col gap-2 items-center">
+            <StyledImage src="/img/ticket-success.svg" />
 
-          <div>
-            <p className="text-[20px] leading-[21px] text-black text-center">
-              Congratulations!
-            </p>
-            <p className="text-[20px] leading-[21px] text-black text-center">
-              Your Ticket purchased is Confirmed
-            </p>
-          </div>
+            <div>
+              <p className="text-[20px] leading-[21px] text-black text-center">
+                Congratulations!
+              </p>
+              <p className="text-[20px] leading-[21px] text-black text-center">
+                Your Ticket purchased is Confirmed
+              </p>
+            </div>
 
-          <div className="w-full flex flex-wrap items-center justify-center my-4 mx-auto gap-10">
-            {transaction?.length > 0 &&
-              transaction?.map(
-                (
-                  { id, batchId, eventId, eventName, userId, ticketId, ticket },
-                  index
-                ) => (
-                  <div
-                    className="w-full max-w-[250px] xl:max-w-[300px] mx-3"
-                    key={id}>
-                    <QRCode
-                      title="title"
-                      value={batchId}
-                      bgColor="#FFFFFF"
-                      fgColor="#000000"
-                      level="L"
-                      className="shadow-[0_3px_10px_rgb(0,0,0,0.2)] p-3 py-7 rounded-[10px]  h-[200px] "
-                    />
-                    <div className="mt-3">
-                      <p className="">
-                        <span className="font-bold">Event:</span> {eventName}
-                      </p>
-                      <p>
-                        <span className="font-bold">Ticket:</span> {ticket}
-                      </p>
-                      <p>
-                        <span className="font-bold">Ticket Id:</span> {ticketId}
-                      </p>
+            <div className="w-full flex flex-wrap items-center justify-center my-4 mx-auto gap-10">
+              {transaction?.length > 0 &&
+                transaction?.map(
+                  (
+                    {
+                      id,
+                      batchId,
+                      eventId,
+                      eventName,
+                      userId,
+                      ticketId,
+                      ticket,
+                    },
+                    index
+                  ) => (
+                    <div
+                      className="w-full max-w-[250px] xl:max-w-[300px] mx-3"
+                      key={id}>
+                      <QRCode
+                        title="title"
+                        value={batchId}
+                        bgColor="#FFFFFF"
+                        fgColor="#000000"
+                        level="L"
+                        className="shadow-[0_3px_10px_rgb(0,0,0,0.2)] p-3 py-7 rounded-[10px]  h-[200px] "
+                      />
+                      <div className="mt-3">
+                        <p className="">
+                          <span className="font-bold">Event:</span> {eventName}
+                        </p>
+                        <p>
+                          <span className="font-bold">Ticket:</span> {ticket}
+                        </p>
+                        <p>
+                          <span className="font-bold">Ticket Id:</span>{" "}
+                          {ticketId}
+                        </p>
+                      </div>
                     </div>
-                  </div>
-                )
-              )}
+                  )
+                )}
+            </div>
           </div>
-        </div>
+        )}
+
+        {success !== true && (
+          <div className="flex flex-col max-w-[380px] gap-5 mx-auto !my-16">
+            <StyledImage src="/img/ticket-fail.svg" className="w-full max-w-[150px] mx-auto"/>
+
+            <div>
+              <p className="text-[16px] leading-[21px] text-gray-700 text-center">
+                Oops! Something went wrong.
+              </p>
+              <p className="text-[16px] leading-[20px] text-gray-700 text-center">
+                Your ticket purchase could not be confirmed. Please try again
+                later.
+              </p>
+            </div>
+          </div>
+        )}
       </Layout>
     </PrivateRoute>
   );
